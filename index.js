efb25a3fd15394bd31ead3152b33999a6661c83f
Bartu10 <jbg10102002@gmail.com>
2023-02-07 23:12:04 +0100

    commmit

--- index.js
+++ index.js
@@ -6,55 +6,61 @@ const teams = {
     "teams": [
       { "id" : 1,
         "name": "Mercedes",
-        "nationality": "German",
+        "nationality": "Germany",
         "drivers": [
           {
             "id" : 1,
             "name": "Lewis Hamilton",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 2,
             "name": "George Russell",
             "number": 63,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Europe"
           }
         ]
       },
       { "id" : 2,
         "name": "Red Bull Racing",
-        "nationality": "Austrian",
+        "nationality": "Austria",
         "drivers": [
           {
             "id" : 3,
             "name": "Max Verstappen",
             "number": 33,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Europe"
           },
           {
             "id" : 4,
             "name": "Sergio Perez",
             "number": 11,
-            "championships": 0
+            "championships": 0,
+            "continent" : "America"
           }
         ]
       },
       { "id" : 3,
         "name": "Ferrari",
-        "nationality": "Italian",
+        "nationality": "Italia",
         "drivers": [
           {
             "id" : 5,
             "name": "Charles Leclerc",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 6,
             "name": "Carlos Sainz",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Europe"
           }
         ]
       },
@@ -66,31 +72,35 @@ const teams = {
             "id" : 7,
             "name": "Esteban Ocon",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 8,
             "name": "Pierre Gasly",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Europe"
           }
         ]
       },
       { "id" : 5,
         "name": "McLaren",
-        "nationality": "British",
+        "nationality": "Great Britain",
         "drivers": [
           {
             "id" : 9,
             "name": "Lando Norris",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 10,
             "name": "Oscar Piastri",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Oceania"
           }
         ]
       },
@@ -102,13 +112,15 @@ const teams = {
             "id" : 11,
             "name": "Valtteri Botas",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 12,
             "name": "Guanyu Zhou",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Asia"
           }
         ]
       },
@@ -120,67 +132,75 @@ const teams = {
             "id" : 13,
             "name": "Kevin Magnussen",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 14,
             "name": "PEB",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "No"
           }
         ]
       },
       { "id" : 8,
         "name": "Alpha Tauri",
-        "nationality": "Italian",
+        "nationality": "Italia",
         "drivers": [
           {
             "id" : 15,
             "name": "Yuri Tsunoda",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Asia"
           },
           {
             "id" : 16,
             "name": "Nyck de Vries",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Europe"
           }
         ]
       },
       { "id" : 9,
         "name": "Aston Martin",
-        "nationality": "British",
+        "nationality": "Great Britain",
         "drivers": [
           {
             "id" : 17,
             "name": "Fernando Alonso",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 18,
             "name": "Lance Stroll",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "Europe"
           }
         ]
       },
       { "id" : 10,
         "name": "Mercedes",
-        "nationality": "German",
+        "nationality": "Germany",
         "drivers": [
           {
             "id" : 19,
             "name": "Alexander Albon",
             "number": 44,
-            "championships": 7
+            "championships": 7,
+            "continent" : "Europe"
           },
           {
             "id" : 20,
             "name": "PEB",
             "number": 77,
-            "championships": 0
+            "championships": 0,
+            "continent" : "No"
           }
         ]
       },
@@ -192,15 +212,33 @@ const teams = {
   });
 
   app.get('/api/teams', (req, res) => {
-    res.send(students);
+    res.send(teams);
   });
 
   app.get('/api/teams/:id', (req, res) => {
-    const team = teams.find(c => c.id === parseInt(req.params.id));
+    const team = teams.teams.find(c => c.id === parseInt(req.params.id));
     if(!team) return res.status(404).send('Equipo no encontrado')
-    else res.send(student)
+    else res.send(team)
 })
 
-  app.get('/api/teams/:country', (req, res) => {
-    
+  app.get('/api/teams/country/:nationality', (req, res) => {
+    const country = teams.teams.filter(c => c.nationality == req.params.nationality);
+    if(!country) return res.status(404).send('Equipo no encontrado')
+    else res.send(country)
   })
+
+  app.get('/api/teams/pilots/:id', (req, res) => {
+    const pilot = teams.teams.find(c => c.drivers.find(e => e.id === parseInt(req.params.id)))
+    if (!pilot) return res.status(404).send('Equipo no encontrado')
+    else res.send(pilot.drivers.find(e => e.id === parseInt(req.params.id)))
+  })
+
+  app.get('/api/teams/pilots/continent/:continent', (req, res) => {
+    const pilot = teams.teams.filter(c => c.drivers.filter(e => e.continent === req.params.continent))
+    if (!pilot) return res.status(404).send('Equipo no encontrado')
+    else res.send(pilot.drivers.filter(e => e.continent === req.params.continent))
+  })     
+
+
+  const port = process.env.port || 80;
+  app.listen(port, () => console.log(`Escuchando en el puerto ${port}...`))
