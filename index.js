d4e2801596351cfc9ce19a80e7a11fe9ee20137c
Javier Bartús Gómez <91955122+Bartu10@users.noreply.github.com>
2024-03-09 22:56:48 +0100

    CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const app = require('./app');
+const cors = require('cors');
 
 // En el puerto decido que escoja el servidor y en caso de que esto no sea asi que elija el 3000 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 
 // Llamo al puerto 3000
 app.listen(port, () => {
